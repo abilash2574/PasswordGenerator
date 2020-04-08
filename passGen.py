e268d2afc640541bcc70490bb616c7c8d48856f7
Abilash <60396912+abilash2574@users.noreply.github.com>
2020-04-08 14:35:58 +0530

    Creation of the core file
    Base

---
+++ passGen.py
@@ -0,0 +1,14 @@
+"""
+    This is a python program that Generates Password with the following features.
+    # A program that generates a random password for the user.
+    # - User input: Length of password, amount of numbers, amount of special characters.
+    # - Mix of upper and lower case numbers
+    # - Minimum of 6 characters
+    # - Instantly copy to clipboard (optional)
+    # - Give it an UI (optional)
+    
+"""
+#------------------Importing Packages-------------------------
+import random, pyperclip
+
+
